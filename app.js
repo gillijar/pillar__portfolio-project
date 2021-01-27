8058dba518b34b359361329b64d19db82955dd25
Jarrod Gilliland <gillijar@gmail.com>
2021-01-26 19:39:10 -0500

    Modal window

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 'use strict';
 
-// Variables
+// Buttons
 const headerBtn = document.querySelector('.btn-primary');
 const likeTripBtn = document.querySelectorAll('.tours__icon');
+const openModalBtn = document.querySelectorAll('.book-now');
+const closeModalBtn = document.querySelector('.modal__close--btn');
+
+const modalWindow = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal__overlay');
 
 // Header button to scroll to main content
 const section1 = document.querySelector('.info');
@@ -16,3 +21,24 @@ likeTripBtn.forEach((btn) => {
     btn.classList.toggle('like');
   });
 });
+
+// Open and close modal
+const openModal = function (e) {
+  // Prevent default
+  e.preventDefault();
+  modalWindow.classList.add('open-modal');
+};
+
+const closeModal = function () {
+  modalWindow.classList.remove('open-modal');
+};
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
+
+openModalBtn.forEach((btn) => btn.addEventListener('click', openModal));
+closeModalBtn.addEventListener('click', closeModal);
+modalOverlay.addEventListener('click', closeModal);
